77d5f9aeb53949f850c5cbfb125a9d425101ca67
Pedro <pedro.barreto.ne@gmail.com>
2023-11-30 10:26:51 -0300

    feat:pronto

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function() {
+setInterval(function () {
   nextImage();
 }, 2500)
 
-function nextImage(){
+function nextImage() {
   count++;
-  if (count > 4){
+  if (count > 4) {
     count = 1;
   }
 
   document.getElementById("radio" + count).checked = true;
 }
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   var header = document.getElementById("header");
   header.classList.toggle("header-aplicado", window.scrollY > 0);
 });
